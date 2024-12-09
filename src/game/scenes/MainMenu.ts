bb40f878f79d9b062d19ec940b8f900eb3a6b1c9
dan-kuroto <2261268657@qq.com>
2024-12-10 00:05:45 +0800

    小球碰撞时融合并加分

--- src/game/scenes/MainMenu.ts
+++ src/game/scenes/MainMenu.ts
@@ -11,7 +11,6 @@ export class MainMenu extends Scene {
     balls: GameObjects.Group;
     currentBall: Physics.Matter.Image | null = null;
 
-    ballCount = 0;
     isDragging = false;
     score = 0;
 
@@ -36,9 +35,8 @@ export class MainMenu extends Scene {
             .setDepth(100);
 
         this.balls = this.add.group({ classType: GameObjects.Image });
-        // 1 ~ 5
-        this.createNewBall();
-        // this.matter.world.on("collisionstart", this.handleCollision, this);
+        this.createNewBall({});
+        this.matter.world.on("collisionstart", this.handleCollision, this);
 
         EventBus.emit("current-scene-ready", this);
     }
@@ -59,7 +57,8 @@ export class MainMenu extends Scene {
             this.time.delayedCall(
                 1000,
                 () => {
-                    this.createNewBall();
+                    // BUG 似乎在连续小球合成的时点击放下小球并创建不会立刻生效？最终会导致同时存在多个待命小球
+                    this.createNewBall({});
                 },
                 [],
                 this
@@ -67,26 +66,61 @@ export class MainMenu extends Scene {
         }
     }
 
-    createNewBall() {
+    // BUG 这里不能用碰撞，要用重叠，否则有时候碰到了不会合并！
+    handleCollision(event, bodyA, bodyB) {
+        const labelA = bodyA.label as string;
+        const labelB = bodyB.label as string;
+        if (labelA.startsWith("ball-") && labelB.startsWith("ball-")) {
+            this.handleFusion(bodyA, bodyB);
+        }
+    }
+
+    handleFusion(bodyA, bodyB) {
+        const labelA = bodyA.label as string;
+        const labelB = bodyB.label as string;
+        if (labelA !== labelB) {
+            return;
+        }
+        const levelA = parseInt(labelA.split("-")[1], 10) as BallLevel;
+        const levelB = parseInt(labelB.split("-")[1], 10) as BallLevel;
+        if (levelA !== levelB || levelA === 11 || levelB === 11) {
+            return;
+        }
+
+        const newLevel = (levelA + 1) as BallLevel;
+        this.increaseScore(levelA + (newLevel === 11 ? 100 : 0));
+        // start fusion
+        const ballA = bodyA.gameObject as Physics.Matter.Image;
+        const ballB = bodyB.gameObject as Physics.Matter.Image;
+        const x = (ballA.x + ballB.x) / 2;
+        const y = (ballA.y + ballB.y) / 2;
+        bodyA.gameObject.destroy();
+        bodyB.gameObject.destroy();
+        this.createNewBall({ level: newLevel, x, y, isStatic: false });
+
+        this.sound.play("biu");
+    }
+
+    createNewBall({
+        level = (Math.floor(Math.random() * 5) + 1) as BallLevel,
+        x = GAME_W / 2,
+        y = 175,
+        isStatic = true,
+    }) {
         // 当小球堆到最顶上的时候会导致跟新创建的小球发生重叠挤压碰撞，但不用担心，以后有了安全线就不会有这个问题了
         // 在那之前就游戏结束了！
-        const level =
-            this.ballCount === 0
-                ? 1
-                : ((Math.floor(Math.random() * 5) + 1) as BallLevel);
-
-        const ball = this.matter.add.image(GAME_W / 2, 175, `ball-${level}`);
+        const ball = this.matter.add.image(x, y, `ball-${level}`);
         ball.setCircle(ball.width / 2, {
             restitution: 0.2,
             friction: 0.1,
             frictionAir: 0.01,
             mass: level * 0.5,
+            label: `ball-${level}`,
         });
-        ball.setStatic(true);
+        ball.setStatic(isStatic);
 
         this.currentBall = ball;
         this.balls.add(ball);
-        this.ballCount++;
         return ball;
     }
 
